48d2774a1644774f4a31be04883186bfb7074cc9
Clarissa Stark <clarissa.stark@gmail.com>
2016-02-22 13:05:29 -0500

    individual cards flip when clicked instead of all cards

--- script.js
+++ script.js
@@ -1,10 +1,68 @@
 var allCards = document.querySelectorAll(".card");
+
+// function randomizeDeck() {
+//   var cardBack = '0123'.split("");
+//   var color = '#';
+//   for (var c = 0; c < allCards.length; c++){
+//     color += cardBack[Math.round(Math.random() * 4)];
+//   }
+//   return color;
+// }
 //
-// allCards.slice(this).addEventListener("click", flip);
+// $(".card").each(function() {
+//     $(this).css("background-color", randomizeDeck());
+// });
+
+// function flipIfClicked() {
+//   if ($(this).hasClass('clickedTag')){
+//     $('.card').toggleClass('flipped');
+//   }else{
+//     alert("Please click on a card.");
+//   }
+// }
+
+$("#card0, #card1, #card2, #card3").click(flip);
+
+// $(".card").click(function(){
+//   (this).attr('class').toggleClass('flipped');
+// }
+// );
 
 function flip() {
-  $('.card').toggleClass('flipped');
+  $(this).toggleClass('flipped');
 }
+
+
+// $('#card1').toggle(
+//     function() {
+//         $('#card1 .card').addClass('flipped');
+//     },
+//     function() { $('#card1').removeClass('flipped');
+//     }
+// );
+//
+// $('#card2').bind('click mouseleave', function() {
+//     $('#card2 .card').toggleClass('flipped');
+// });
+//
+// $('#card3').bind({
+//     click: function() {
+//         $('#card3 .card').toggleClass('flipped');
+//     },
+//     mouseleave: function() {
+//         $('#card3 .card').toggleClass('flipped');
+//     }
+// });
+
+// ('#card4').bind({
+//     click: function() {
+//         $('#card4 .card').toggleClass('flipped');
+//     },
+//     mouseleave: function() {
+//         $('#card4 .card').toggleClass('flipped');
+//     }
+// });
+
 //
 // $('.flip').click(function(){
 //      $(this).find('.card').addClass('flipped').toggle(function(){
@@ -12,7 +70,6 @@ function flip() {
 //      });
 //      return false;
 //  });
-
 
 
 
