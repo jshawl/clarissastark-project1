57b81555b4b3d57dde2afe148ebbabb50127c914
Clarissa Stark <clarissa.stark@gmail.com>
2016-02-22 12:21:00 -0500

    cards all have a front and back

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
+var allCards = document.querySelectorAll(".card");
+//
+// allCards.slice(this).addEventListener("click", flip);
 
-/*pseudocoding
+function flip() {
+  $('.card').toggleClass('flipped');
+}
+//
+// $('.flip').click(function(){
+//      $(this).find('.card').addClass('flipped').toggle(function(){
+//          $(this).removeClass('flipped');
+//      });
+//      return false;
+//  });
+
+
+
+
+/* pseudocoding
 
 +Think about not having too many global variables
 
 var gameCard =
++Start game
+randomly assign colors to cells
+click event on cell reveals the color
+
+scoreing
+if click event on cell value = click event on second cell value, score increases by one
+
 
 */
-    
